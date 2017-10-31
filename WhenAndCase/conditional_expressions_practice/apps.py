ade9bc96e14afff41eb105e70964723ba42cff88
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-10-31 16:37:02 +0900

    conditional_expressions_practice app 추가

---
+++ WhenAndCase/conditional_expressions_practice/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConditionalExpressionsPracticeConfig(AppConfig):
+    name = 'conditional_expressions_practice'
--- WhenAndCase/config/settings.py
+++ WhenAndCase/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'conditional_expressions_practice',
+
 ]
 
 MIDDLEWARE = [
